14a0844deaeb39bc37590b1cabdf923a6bcc97ed
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-06-30 12:29:15 +0530

    updated

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import ttk
+import threading
+
+from pynput import keyboard
 
 from iris_cli import iris_cli, DEFAULT_BRIGHT, DEFAULT_TEMP, reset_cli
 
@@ -12,11 +15,12 @@ class SliderApp:
         self.root.title("Temperature and Brightness Control")
 
         # Initialize StringVars for labels
-        self.temperature_label_var = tk.StringVar()
-        self.temperature_label_var.set("Temperature: 6500 K")
-
-        self.brightness_label_var = tk.StringVar()
-        self.brightness_label_var.set("Brightness: 100 %")
+        self.temperature_label_var = tk.StringVar(
+            value=f"Temperature: {DEFAULT_TEMP} K"
+        )
+        self.brightness_label_var = tk.StringVar(
+            value=f"Brightness: {DEFAULT_BRIGHT} %"
+        )
 
         # Initialize IntVars for sliders
         self.temperature_value = tk.IntVar(value=DEFAULT_TEMP)
@@ -32,13 +36,11 @@ class SliderApp:
         reset_button.pack(pady=10)
 
         # Create and place the temperature label
-        self.temperature_label = ttk.Label(
-            root, textvariable=self.temperature_label_var
-        )
-        self.temperature_label.pack(pady=10)
+        temperature_label = ttk.Label(root, textvariable=self.temperature_label_var)
+        temperature_label.pack(pady=10)
 
         # Create and place the temperature slider
-        self.temperature_slider = ttk.Scale(
+        temperature_slider = ttk.Scale(
             root,
             from_=1000,
             to=10000,
@@ -47,14 +49,14 @@ class SliderApp:
             command=self.on_temperature_change,
             length=500,
         )
-        self.temperature_slider.pack(pady=10, padx=10)
+        temperature_slider.pack(pady=10, padx=10)
 
         # Create and place the brightness label
-        self.brightness_label = ttk.Label(root, textvariable=self.brightness_label_var)
-        self.brightness_label.pack(pady=10)
+        brightness_label = ttk.Label(root, textvariable=self.brightness_label_var)
+        brightness_label.pack(pady=10)
 
         # Create and place the brightness slider
-        self.brightness_slider = ttk.Scale(
+        brightness_slider = ttk.Scale(
             root,
             from_=10,
             to=100,
@@ -63,7 +65,7 @@ class SliderApp:
             command=self.on_brightness_change,
             length=350,
         )
-        self.brightness_slider.pack(pady=10, padx=10)
+        brightness_slider.pack(pady=10, padx=10)
 
     def on_temperature_change(self, _=None):
         """Function to update the temperature label when the slider value changes"""
@@ -86,12 +88,59 @@ class SliderApp:
         reset_cli()
 
 
+class KeboardShortcut:
+    """Handling Keyboard Shortcuts and synchronizing with gui"""
+
+    def __init__(self) -> None:
+        self.ctrl_pressed = False
+
+        self.brigh_step = 5
+        self.temp_step = 100
+
+    def on_press(self, key):
+        """on press"""
+
+        try:
+            if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+                self.ctrl_pressed = True
+
+            if self.ctrl_pressed:
+                if key == keyboard.Key.f8:
+                    print("Decrease")
+                elif key == keyboard.Key.f9:
+                    print("Increase")
+        except AttributeError:
+            pass
+
+    def on_release(self, key):
+        """on release"""
+        try:
+            if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+                self.ctrl_pressed = False
+        except AttributeError:
+            pass
+
+    def start_listener(self):
+        """start"""
+        with keyboard.Listener(
+            on_press=self.on_press, on_release=self.on_release
+        ) as listener:
+            listener.join()
+
+
 if __name__ == "__main__":
     try:
         reset_cli()  # This might change in future if we want to tsave
         app_root = tk.Tk()
         app = SliderApp(app_root)
+
+        key_obj = KeboardShortcut()
+        th = threading.Thread(target=key_obj.start_listener)
+
+        th.daemon = True
+        th.start()
         app_root.mainloop()
+        # pylint: disable=broad-exception-caught
     except Exception as e:
         pass
     finally:
